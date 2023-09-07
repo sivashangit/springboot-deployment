ad2d54ff7c93335fc58762e68df6e4d26d793282
sivakumar <sivaktvr@gmail.com>
2023-09-08 04:31:11 +0530

    first commit

---
+++ src/test/java/com/example/springbootdeployment/SpringbootDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
